ba271a0145a930d131d0b1e68d997058580b30ae
Kevin Jiang <kjiang1998@gmail.com>
2018-01-12 03:22:54 -0600

    Refactored UI code and added ViewModel, more to come

---
+++ LolApp/MainViewModel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using LolApp.Data;
+using LolApp.Api;
+using System.Windows.Media.Imaging;
+using System.ComponentModel;
+
+namespace LolApp
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private RiotApi api;
+        private StaticApi staticApi;
+
+        public ObservableCollection<Region> RegionList { get; set; }
+
+        public string Username { get; set; }
+        public Region Region { get; set; }
+
+        private Summoner summoner;
+        public Summoner Summoner
+        {
+            get { return summoner; }
+            set
+            {
+                if (summoner != value)
+                {
+                    summoner = value;
+                    RaisePropertyChanged("Summoner");
+                }
+            }
+        }
+        public BitmapImage ProfileIcon { get; set; }
+
+        public MainViewModel(RiotApi apiInstance, StaticApi staticApiInstance)
+        {
+            api = apiInstance;
+            staticApi = staticApiInstance;
+
+            RegionList = new ObservableCollection<Region>();
+            RegionList.Add(new Region("NA", "na1"));
+            RegionList.Add(new Region("EUW", "euw1"));
+            RegionList.Add(new Region("EUN", "eun1"));
+            RegionList.Add(new Region("KR", "kr"));
+            Region = RegionList[0];
+        }
+
+        public void GetInfo()
+        {
+            // get a player from name input
+            Summoner = api.GetSummonerByName(Region, Username);
+
+            // get profile icon from static api
+            string profileIconUrl = staticApi.GetProfileIconUrl(Summoner.ProfileIconId, Region);
+
+            var uri = new Uri(profileIconUrl);
+            ProfileIcon = new BitmapImage(uri);
+
+            List<LeaguePosition> leagues = api.GetLeaguePositionById(Region, Summoner.Id);
+            string tierIconFormat = "{0}_{1}.png";
+
+            //// check validity of name
+            //if (Regex.IsMatch(name, "^[0-9\\p{L} _\\.]+$"))
+            //{
+            //    throw new Exception("Invalid username");
+            //}
+
+            //try
+            //{
+            //    PopulateInfo();
+            //}
+            //// if exception is HTTP error code
+            //catch (WebException ex) when (ex.Response is HttpWebResponse response)
+            //{
+            //    lblStatus.Text = response.StatusDescription;
+            //}
+            //catch (Exception ex)
+            //{
+            //    lblStatus.Text = ex.ToString();
+            //}
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            // take a copy to prevent thread issues
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
--- LolApp/MainWindow.xaml.cs
+++ LolApp/MainWindow.xaml.cs
@@ -27,111 +27,21 @@ namespace LolApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string apiKey;
-        RiotApi api;
-        StaticApi staticApi;
+        private MainViewModel vm;
+        RiotApi riotApi = new RiotApi(ConfigurationManager.AppSettings["apiKey"]);
+        StaticApi staticApi = new StaticApi(ConfigurationManager.AppSettings["apiKey"]);
 
         public MainWindow()
         {
             InitializeComponent();      // initialize window and initial components
-            InitializeScreen();         // populate items
-        }
 
-        private void InitializeScreen()
-        {
-            cbxRegion.Items.Add(new Region("NA","na1"));   
-            cbxRegion.Items.Add(new Region("EUW", "euw1"));  
-            cbxRegion.Items.Add(new Region("EUN", "eun1"));
-            cbxRegion.Items.Add(new Region("KR", "kr"));
-            cbxRegion.SelectedIndex = 0;
-            
-
-            apiKey = ConfigurationManager.AppSettings["apiKey"];    // get API key from config file
-            api = new RiotApi(apiKey);                              // create a new instance of the Riot API
-            staticApi = new StaticApi(apiKey);                      // create a new instance of the Riot Static API
-
-        }
-
-        private void PopulateInfo()
-        {
-            string name = txtUsername.Text;
-
-            // get a player from name input
-            var region = (Region)cbxRegion.SelectedItem;
-            Summoner player = api.GetSummonerByName(region, name);
-
-            // put player info into labels
-            lblName.Text = player.Name;
-            lblLevel.Text = "Level " + player.SummonerLevel.ToString();
-            lblRevision.Text = Api.Api.FromUnixTime(player.RevisionDate).ToString();
-            lblStatus.Text = player.Id.ToString();
-
-            // get profile icon from static api
-            string profileIconUrl = staticApi.GetProfileIconUrl(player.ProfileIconId, region);
-
-            var uri = new Uri(profileIconUrl);
-            var bitmap = new BitmapImage(uri);
-            imgProfileIcon.Source = bitmap;
-
-            List<LeaguePosition> leagues = api.GetLeaguePositionById(region, player.Id);
-            string tierIconFormat = "{0}_{1}.png";
-
-            for (int i = 0; i < leagues.Count; ++i)
-            {
-                // Adds a column if there is more than one queue the summoner is ranked in
-                if (i > 0)
-                {
-                    grdLeagues.ColumnDefinitions.Add(new ColumnDefinition());
-                }
-
-                Image tierIcon = new Image();
-                string iconName = String.Format(tierIconFormat, leagues[i].Tier.ToLower(), leagues[i].Rank.ToLower());
-                tierIcon.Source = new BitmapImage(
-                    new Uri("pack://application:,,,/LolApp;component/resources/tier-icons/" + iconName));
-                Grid.SetRow(tierIcon, 0);
-                Grid.SetColumn(tierIcon, i);
-
-
-                TextBlock queueType = new TextBlock();
-                queueType.Text = leagues[i].QueueType;
-                Grid.SetRow(queueType, 1);
-                Grid.SetColumn(queueType, i);
-
-
-                TextBlock rank = new TextBlock();
-                rank.Text = leagues[i].Tier + leagues[i].Rank;
-                Grid.SetRow(rank, 2);
-                Grid.SetColumn(rank, i);
-
-                grdLeagues.Children.Add(tierIcon);
-                grdLeagues.Children.Add(queueType);
-                grdLeagues.Children.Add(rank);
-            }
-
-            grdLeagues.ColumnDefinitions.Add(new ColumnDefinition());
-
-            //// check validity of name
-            //if (Regex.IsMatch(name, "^[0-9\\p{L} _\\.]+$"))
-            //{
-            //    throw new Exception("Invalid username");
-            //}
+            vm = new MainViewModel(riotApi, staticApi);
+            DataContext = vm;
         }
 
         private void BtnGetInfo_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                PopulateInfo();
-            }
-            // if exception is HTTP error code
-            catch (WebException ex) when (ex.Response is HttpWebResponse response)
-            {
-                lblStatus.Text = response.StatusDescription;
-            }
-            catch (Exception ex)
-            {
-                lblStatus.Text = ex.ToString();
-            }
+            vm.GetInfo();
         }
     }
 }
